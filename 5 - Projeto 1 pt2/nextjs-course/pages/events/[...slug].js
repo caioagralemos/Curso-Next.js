2f12b71b0e47eb20ed72c2f242c1ad304c42edd5
caioagralemos <caioagralemos@live.com>
2023-06-26 15:17:53 -0300

    Update [...slug].js

--- 5 - Projeto 1 pt2/nextjs-course/pages/events/[...slug].js
+++ 5 - Projeto 1 pt2/nextjs-course/pages/events/[...slug].js
@@ -1,16 +1,47 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import EventList from "../../components/events/eventList";
 import { getFilteredEvents } from "../../components/data/api-util";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
 export default function FilterEventPage(props) {
-  const router = useRouter()
+  const router = useRouter();
   const events = props.filteredEvents;
+  const [loadedEvents, setLoadedEvents] = useState(events);
 
-  if (!events) {
+  const { data, error } = useSWR(
+    "https://nextjs-course-13a42-default-rtdb.firebaseio.com/events.json"
+  );
+
+  useEffect(() => {
+    if (data) {
+      var eventsArray = [];
+
+      for (const key in data) {
+        eventsArray.push({
+          id: key,
+          ...data[key],
+        });
+      }
+
+      var year = +router.query.slug[0];
+      var month = +router.query.slug[1];
+    
+      let filteredEvents = eventsArray.filter((event) => {
+        const eventDate = new Date(event.date);
+        return (
+          eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+        );
+      });
+
+      setLoadedEvents(filteredEvents);
+    }
+  }, [data]);
+
+  if (loadedEvents.length == 0) {
     return (
-      <div>
-        <h1>No events found</h1>
+      <div className="center">
+        <h1>Não foram encontrados eventos para essa data</h1>
       </div>
     );
   }
@@ -26,7 +57,7 @@ export default function FilterEventPage(props) {
           : `${router.query.slug[1]}`}
         /{router.query.slug[0]}
       </h3>
-      <EventList items={events} />
+      <EventList items={loadedEvents} />
     </Fragment>
   );
 }

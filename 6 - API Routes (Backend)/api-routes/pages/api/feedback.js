ee74d70e049a4f9b7daaa7383e9cb25009760911
caioagralemos <caioagralemos@live.com>
2023-06-28 12:36:33 -0300

    6 - API Routes & Backend

--- 6 - API Routes (Backend)/api-routes/pages/api/feedback.js
+++ 6 - API Routes (Backend)/api-routes/pages/api/feedback.js
@@ -1,3 +1,28 @@
+import { v4 } from "uuid";
+import fs from "fs";
+import path from "path";
+
 export default function handler(req, res) {
-  res.status(200).json({ message: "Funcionou!" });
+  if (req.method == "POST") {
+    const email = req.body.email;
+    const feedbackText = req.body.feedback;
+
+    const newFeedback = {
+      id: v4(),
+      email: email,
+      text: feedbackText,
+    };
+
+    // guardando os dados
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    var data = JSON.parse(fileData);
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res
+      .status(201)
+      .json({ message: "Feedback registrado!", feedback: newFeedback });
+  } else {
+    res.redirect('/')
+  }
 }
--- 6 - API Routes (Backend)/api-routes/pages/index.js
+++ 6 - API Routes (Backend)/api-routes/pages/index.js
@@ -1,7 +1,48 @@
+import { useRef } from "react";
+
 export default function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(evt) {
+    evt.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    const reqBody = { email: enteredEmail, feedback: enteredFeedback }
+
+    if (
+      enteredEmail == "" ||
+      enteredFeedback == "" ||
+      (enteredEmail == "" && enteredFeedback == "")
+    ) {
+      alert("Um ou mais campos estão vazios!");
+    } else {
+      fetch("/api/feedback", {
+        method: "POST",
+        body: JSON.stringify(reqBody),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((response) => response.json())
+        .then((data) => console.log(data))
+    }
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Seu email</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="email">Dê sua opinião</label>
+          <textarea rows="5" id="feedback" ref={feedbackInputRef} />
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }

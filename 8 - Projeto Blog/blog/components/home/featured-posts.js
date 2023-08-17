1245962dc04f1df096e96161035e937ea50b4442
caioagralemos <caioagralemos@live.com>
2023-08-17 11:36:27 -0300

    8 - Projeto Blog

--- 8 - Projeto Blog/blog/components/home/featured-posts.js
+++ 8 - Projeto Blog/blog/components/home/featured-posts.js
@@ -1,5 +1,8 @@
 import './featured-posts.module.css'
 
 export default function FeaturedPosts() {
-    return <div></div>
+    return <section className={classes.latest}>
+        <h2>Featured Posts</h2>
+        
+    </section>
 }
---
+++ 8 - Projeto Blog/blog/components/layout/logo.js
@@ -0,0 +1,6 @@
+import classes from './logo.module.css'
+export default function Logo() {
+    return <div className={classes.logo}>
+        Caio's Next Blog
+    </div>
+}

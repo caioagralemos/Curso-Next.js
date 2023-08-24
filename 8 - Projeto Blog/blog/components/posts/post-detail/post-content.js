69cde133d99b390a5d2a7236a59b9e8dfafe2212
caioagralemos <caioagralemos@live.com>
2023-08-23 22:49:47 -0300

    8 - Projeto Blog

---
+++ 8 - Projeto Blog/blog/components/posts/post-detail/post-content.js
@@ -0,0 +1,12 @@
+import PostHeader from "./post-header";
+
+const DUMMY_POST= {
+    slug: ''
+}
+
+export default function PostContent(props) {
+    return <article>
+        <PostHeader title="" image=""/>
+        CONTENT
+    </article>
+}
